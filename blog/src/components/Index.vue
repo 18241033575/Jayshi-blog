416bf1e714745fbba84f3162d39870b2c5f4f088
Jayshi <240638896@qq.com>
2019-07-23 15:22:37 +0800

    element-ui

---
+++ blog/src/components/Index.vue
@@ -0,0 +1,24 @@
+<template>
+  <!-- 首页 -->
+  <div id="main">
+    <head-nav></head-nav>
+    Hello Word!
+  </div>
+</template>
+
+<script>
+  import headNav from './Header/Header';
+  export default {
+    components:{
+        headNav
+    },
+    name: "Index"
+  }
+</script>
+
+<style scoped>
+   *{
+     margin: 0;
+     padding: 0;
+   }
+</style>
--- blog/src/main.js
+++ blog/src/main.js
@@ -3,9 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import elementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 
-
-Vue.use(bjs);
+Vue.use(elementUI);
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
